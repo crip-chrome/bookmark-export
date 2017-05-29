b6388b7c2adebb98fb981dbb0575eaeaafd7a0a9
Igors Krasjukovs <tahq69@gmail.com>
2017-05-29 20:03:01 +0300

    initial configuration and build process rafactoring;

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,56 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-var plugins = [
-    new ExtractTextPlugin('../css/styles.css', {
-        allChunks: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js', Infinity)
-];
-
-if (process.argv.indexOf('--minimize') !== -1) {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}));
-}
-
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
-    entry: {
-        bundle: APP_DIR + '/entry.es6',
-        background: APP_DIR + '/background.es6',
-        styles: APP_DIR + '/sass/app.scss',
-        vendor: ['vue', 'vue-router', 'vuex', 'vuex-router-sync']
-    },
-    output: {
-        path: BUILD_DIR + '/js',
-        filename: '[name].js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue',
-                options: {
-                    postcss: [
-                        require('autoprefixer')({
-                            browsers: ['last 3 versions']
-                        })
-                    ]
-                }
-            },
-            {
-                test: /\.es6$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'url',
-                options: {
-                    limit: 10000,
-                    name: '[name].[ext]?[hash]'
-                }
-            }
-        ]
-    },
-    plugins: plugins,
-    vue: {
-        loaders: {
-            js: 'babel'
+  devtool: '#source-map',
+  entry: {
+    bundle: APP_DIR + '/entry.js',
+    background: APP_DIR + '/background.js',
+    styles: APP_DIR + '/sass/app.scss',
+    vendor: ['vue', 'vue-router', 'vuex', 'vuex-router-sync']
+  },
+  output: {
+    path: BUILD_DIR + '/js',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {enforce: 'pre', test: /\.js$|\.vue$/, loader: 'eslint-loader', exclude: /node_modules/, query: {fix: true}},
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        exclude: /node_modules/,
+        options: {
+          loaders: {
+            js: 'babel-loader',
+            i18n: '@kazupon/vue-i18n-loader'
+          }
         }
-    }
+      },
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, loader: 'url-loader'},
+      {
+        test: /\.scss$|\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('../css/styles.css', {allChunks: true}),
+    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'})
+  ]
 };
+
+if (process.env.NODE_ENV === '') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+  );
+}
